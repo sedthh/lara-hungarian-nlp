40a3c42e69b7edad500a5d1e29aa16f24dd1a7f1
sedthh <sedthh@gmail.com>
2017-04-13 19:19:10 +0200

    - improved common intent list

--- lara/parser.py
+++ lara/parser.py
@@ -318,10 +318,10 @@ def match_intents(fast_intent, text=""):
 def get_common_intents():
 	return {
 		"_negative"		: [{"stem":"nem"},{"stem":"ne"},{"stem":"soha"},{"stem":"mégse","affix":["m"]}],
-		"_positive"		: [{"stem":"igen"},{"stem":"aha"},{"stem":"ja"},{"stem":"ok","affix":["é","s"]}],
-		"_greeting" 	: [{"stem":"hi","match_at":"start"},{"stem":"szia","match_at":"start"},{"stem":"helló","match_at":"start"},{"stem":"szervusz","match_at":"start"},{"stem":"hali","match_at":"start"}],
+		"_positive"		: [{"stem":"igen"},{"stem":"aha"},{"stem":"ja","affix":["ja","h"]},{"stem":"ok","affix":["é","s","és","sa","ay"]}],
+		"_greeting" 	: [{"stem":"hi","match_at":"start"},{"stem":"hai","match_at":"start"},{"stem":"szia","match_at":"start"},{"stem":"helló","match_at":"start","affix":["ka"]},{"stem":"szervusz","match_at":"start"},{"stem":"szerbusz","match_at":"start"},{"stem":"szevasz","match_at":"start"},{"stem":"hali","match_at":"start","affix":["hó"]},{"stem":"jó napot"},{"stem":"jó reggelt"}],
 		"_leaving" 		: [{"stem":"bye","match_at":"end"},{"stem":"szia","match_at":"end"},{"stem":"viszlát"},{"stem":"viszont látásra"},{"stem":"jó éj","affix":["t","szakát"]}],
-		"_thanking"		: [{"stem":"kösz","affix":["i","önöm","önjük","önet"]},{"stem":"thx"},{"stem":"thank"}],
+		"_thanking"		: [{"stem":"kösz","affix":["i","önöm","önjük","önet","ike","csi"]},{"stem":"kössz"},{"stem":"kösszentyű"},{"stem":"thx"},{"stem":"thank"}],
 		"_command"		: [{"stem":"keres(s|d)","wordclass":"regex"},{"stem":"mutass(s|d)","wordclass":"regex"},{"stem":"mond(j|d)","wordclass":"regex"},{"stem":"szeretné(k|m)","wordclass":"regex"},{"stem":"akaro(k|m)","wordclass":"regex"}],
 		"_question"		: [{"stem":"\?+($|\s\w+)","wordclass":"regex"},{"stem":"([^,][^,\S+]hogy|^hogy)(an)?","wordclass":"regex"},{"stem":"hol"},{"stem":"honnan"},{"stem":"hová"},{"stem":"hány","affix":["an","at","ból"]},{"stem":"mettől"},{"stem":"meddig"},{"stem":"merre"},{"stem":"mennyi","affix":["en","re"]},{"stem":"mi","affix":["t","k","ket","kor","korra","lyen","lyenek","nek","től","kortól","korra","ből","hez","re","vel"]},{"stem":"ki","affix":["t","k","ket","nek","knek","től","ktől","ből","kből","hez","re","kre","vel","kkel"]}],
 		"_conditional"	: [{"stem":"volna"},{"stem":"lenne"},{"stem":"\w+h[ae]t\w+","wordclass":"regex"}]
