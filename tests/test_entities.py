62d2fb84eb4ec4afedb527ca4ad7d1b5013481c4
sedthh <sedthh@gmail.com>
2018-03-29 15:00:53 +0200

    - added 'tone' to entities
    - added 'tone' to entities to detect formal or informal questions
    - added 'tone' to test_entities
    - test_entities now checks for wrong boundary declarations
    - improved entities based on user feedback

--- tests/test_entities.py
+++ tests/test_entities.py
@@ -20,12 +20,14 @@ def validate_intent(intents):
 			if 'stem' not in item:
 				print(intent,'missing "stem" key')
 			else:
+				if '\b' in item['stem'] and ('boundary' not in item or item['boundary']):
+					print(intent,'has bounary set but has \\b declared as regular expression')
 				if any(test in item['stem'] for test in is_regex):
 					if 'wordclass' not in item or item['wordclass']!='regex':
 						print(intent,'probably has a regex "wordclass" declared otherwise in',item['stem'])
 
 @pytest.mark.parametrize("entity", [
-    "common","commands","counties","dow","smalltalk","popculture","emoji","disallow"
+    "common","commands","counties","dow","smalltalk","popculture","emoji","disallow","tone"
 ])
 def test_entities(entity):
 	parenthesis_check = eval('parser.Intents(entities.'+entity+'()).match_set("test")')
