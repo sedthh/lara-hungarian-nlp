4fac57bb36510d1699832a5aef917f91550170f3
sedthh <sedthh@gmail.com>
2018-03-29 18:35:41 +0200

    - improved entities
    - test_entities now alerts if regular expressions have double letters in them or vowels with accents alone
    - fixed entities accordingly

--- tests/test_entities.py
+++ tests/test_entities.py
@@ -22,6 +22,25 @@ def validate_intent(intents):
 			else:
 				if '\b' in item['stem'] and ('boundary' not in item or item['boundary']):
 					print(intent,'has bounary set but has \\b declared as regular expression')
+				if 'wordclass' in item and item['wordclass']=='regex':
+					switch	= False
+					last		= ''
+					for char in item['stem']:
+						if char=='[':
+							switch	= True
+						elif char==']':
+							switch	= False
+						elif char in ('á','Á','é','É','í','Í','ü','Ü','ű','Ű','ú','Ú','ö','Ö','ő','Ő','ó','Ó'):
+							if not switch:
+								print(intent,'has accents declared in regular expression without counterparts:',item['stem'])
+								break
+						elif char.isalpha() and char==last:
+								print(intent,'has double letters in regular expression:',item['stem'])
+								break
+						if last=='\\':
+							last	= last+char
+						else:
+							last	= char
 				if any(test in item['stem'] for test in is_regex):
 					if 'wordclass' not in item or item['wordclass']!='regex':
 						print(intent,'probably has a regex "wordclass" declared otherwise in',item['stem'])
