7f12f449de6a24f9ce324fafb94085292ee14f6e
sedthh <sedthh@gmail.com>
2018-04-03 12:59:56 +0200

    - updated entities to match certain typos
    - updated test_entities to check "inc" and "exc" for possibly wrong declarations
    - updated entities based on failure messages given by test_entities

--- tests/test_entities.py
+++ tests/test_entities.py
@@ -5,46 +5,56 @@ import os.path, sys
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
 from lara import parser, entities
 
+valid_keys	= set(['stem','clean_stem','affix','clean_affix','prefix','clean_prefix','wordclass','inc','exc','score','clean_score','match_stem','ignorecase','boundary','max_words'])
+valid_class = set(['noun','verb','adjective','regex','emoji','special'])
+is_regex	= set(['|','(',')','+','*','+','?','\\','[',']','{','}'])
+	
 def validate_intent(intents):
-	valid_keys	= set(['stem','clean_stem','affix','clean_affix','prefix','clean_prefix','wordclass','inc','exc','score','clean_score','match_stem','ignorecase','boundary','max_words'])
-	valid_class = set(['noun','verb','adjective','regex','emoji','special'])
-	is_regex	= set(['|','(',')','+','*','+','?','\\','[',']','{','}'])
 	for intent,declaration in intents.items():
 		for item in declaration:
-			for key,value in item.items():
-				if key not in valid_keys:
-					print(intent,'has unknown key:',key)
-			if 'wordclass' in item:
-				if item['wordclass'] not in valid_class:
-					print(intent,'has invalid "wordclass" declared')
-			if 'stem' not in item:
-				print(intent,'missing "stem" key')
-			else:
-				if '\b' in item['stem'] and ('boundary' not in item or item['boundary']):
-					print(intent,'has bounary set but has \\b declared as regular expression')
-				if 'wordclass' in item and item['wordclass']=='regex':
+			validate_intent_item(item,intent)
+			if 'inc' in item:
+				for sub_item in item['inc']:
+					validate_intent_item(sub_item,intent)
+			if 'exc' in item:
+				for sub_item in item['exc']:
+					validate_intent_item(sub_item,intent)
+			
+def validate_intent_item(item,intent):
+	for key in item:
+		if key not in valid_keys:
+			print(intent,'has unknown key:',key)
+	if 'wordclass' in item:
+		if item['wordclass'] not in valid_class:
+			print(intent,'has invalid "wordclass" declared')
+	if 'stem' not in item:
+		print(intent,'missing "stem" key')
+	else:
+		if '\b' in item['stem'] and ('boundary' not in item or item['boundary']):
+			print(intent,'has bounary set but has \\b declared as regular expression')
+		if 'wordclass' in item and item['wordclass']=='regex':
+			switch	= False
+			last		= ''
+			for char in item['stem']:
+				if char=='[':
+					switch	= True
+				elif char==']':
 					switch	= False
-					last		= ''
-					for char in item['stem']:
-						if char=='[':
-							switch	= True
-						elif char==']':
-							switch	= False
-						elif char in ('á','Á','é','É','í','Í','ü','Ü','ű','Ű','ú','Ú','ö','Ö','ő','Ő','ó','Ó'):
-							if not switch:
-								print(intent,'has accents declared in regular expression without counterparts:',item['stem'])
-								break
-						elif char.isalpha() and char==last:
-								print(intent,'has double letters in regular expression:',item['stem'])
-								break
-						if last=='\\':
-							last	= last+char
-						else:
-							last	= char
-				if any(test in item['stem'] for test in is_regex):
-					if 'wordclass' not in item or item['wordclass']!='regex':
-						print(intent,'probably has a regex "wordclass" declared otherwise in',item['stem'])
-
+				elif char in ('á','Á','é','É','í','Í','ü','Ü','ű','Ű','ú','Ú','ö','Ö','ő','Ő','ó','Ó'):
+					if not switch:
+						print(intent,'has accents declared in regular expression without counterparts:',item['stem'])
+						break
+				elif char.isalpha() and char==last:
+						print(intent,'has double letters in regular expression:',item['stem'])
+						break
+				if last=='\\':
+					last	= last+char
+				else:
+					last	= char
+		if any(test in item['stem'] for test in is_regex):
+			if 'wordclass' not in item or item['wordclass']!='regex':
+				print(intent,'probably has a regex "wordclass" declared otherwise in',item['stem'])
+						
 @pytest.mark.parametrize("entity", [
     "common","commands","counties","dow","smalltalk","cocktail","popculture","emoji","disallow","tone"
 ])
